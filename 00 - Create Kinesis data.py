06fa9dde2f7563bae9e2a99dc6a35c581935e08d
volker.tjaden@databricks.com <volker.tjaden@databricks.com>
2021-03-30 14:44:51 +0000

    Finished 01 Wind Turbine ingestion

---
+++ 00 - Create Kinesis data.py
@@ -0,0 +1,35 @@
+# Databricks notebook source
+# MAGIC %run ./00-setup_power $reset_all="False"
+
+# COMMAND ----------
+
+import boto3
+import json
+from time import sleep
+
+kinesis_client = boto3.client('kinesis', region_name='us-west-2')
+
+# COMMAND ----------
+
+df = spark.read.format('delta').load(path+"/turbine/kinesis_sample/")
+
+# COMMAND ----------
+
+while True:
+  df_sample = df.sample(fraction=0.01).limit(500)
+  pdf = df_sample.toPandas()
+
+  Records = []
+  for index, row in pdf.iterrows():
+      Records.append(
+      {
+        'Data': row['value'],
+        'PartitionKey': str(int(row['key']))
+      })
+
+  response = kinesis_client.put_records(
+      Records=Records,
+      StreamName=my_stream_name
+  )
+  
+  sleep(5)
